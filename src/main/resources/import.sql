04996a93ec7b4a9a755fdeab1fa9aaea797fbca8
Gunnar Ingi Stefansson <gis20@hi.is>
2019-10-18 21:43:54 +0000

    adds import.sql file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+INSERT INTO USERS(id, password, username) VALUES (1, '12345', 'notadmin');
+INSERT INTO USERS(id, password, username) VALUES (2, '12345', 'gunnar');
+INSERT INTO USERS(id, password, username) VALUES (3, '12345', 'jon');
+INSERT INTO USERS(id, password, username) VALUES (4, '12345', 'someone');
+INSERT INTO USERS(id, password, username) VALUES (5, '12345', 'dude');
+INSERT INTO USERS(id, password, username) VALUES (6, '12345', 'dudette');
