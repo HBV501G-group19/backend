b359fd6f80a7bcc9b6eab9ffc791060027369628
Gunnar Ingi Stefansson <gis20@hi.is>
2019-11-08 15:08:53 +0000

    Puts up a directions endpoint

--- src/main/java/is/hi/hbvproject/controller/OrsController.java
+++ src/main/java/is/hi/hbvproject/controller/OrsController.java
@@ -1,14 +1,22 @@
 package is.hi.hbvproject.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.wololo.geojson.GeoJSONFactory;
+import org.wololo.geojson.Point;
+import org.wololo.geojson.FeatureCollection;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.RequestBody;
 
 import is.hi.hbvproject.service.OrsService;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 
 @RestController
 @CrossOrigin
@@ -21,11 +29,40 @@ public class OrsController {
   }
   
   @RequestMapping(
-    value = "/ors",
+    value = "/ors/geocode",
     method = RequestMethod.GET,
     produces = "application/json"
   )
   public String getGeoCodes(@RequestParam(value = "geocode") String geoCode) {
     return orsService.getGeoCodes(geoCode).toString();
+  }
+
+  @RequestMapping(
+    value = "/ors/directions",
+    method = RequestMethod.POST,
+    produces = "application/json",
+    consumes = "application/json"
+  )
+  public List<FeatureCollection> getDirections(@RequestBody String json) {
+    JSONObject body = new JSONObject(json);
+    JSONArray endpointsArray = body.getJSONArray("endpoints");
+    List<FeatureCollection> paths = new ArrayList<>();
+    for (int i = 0; i < endpointsArray.length(); i++) {
+      
+      String eString = endpointsArray.get(i).toString();
+      JSONObject endpoints = new JSONObject(eString);
+
+ 
+      String o = endpoints.get("origin").toString();
+      Point origin = (Point) GeoJSONFactory.create(o);
+      String d = endpoints.get("destination").toString();
+      Point destination = (Point) GeoJSONFactory.create(d);
+      String profile = endpoints.getString("profile");
+
+      FeatureCollection path = orsService.getDirections(origin, destination, profile);
+      paths.add(path);
+    }
+
+    return paths;
   }
 }
--- src/main/java/is/hi/hbvproject/service/OrsService.java
+++ src/main/java/is/hi/hbvproject/service/OrsService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.geolatte.geom.G2D;
 import org.wololo.geojson.FeatureCollection;
+import org.wololo.geojson.Point;
 
 import kong.unirest.json.JSONArray;
 
 public interface OrsService {
   List<org.geolatte.geom.Polygon<G2D>> getIsochrones(JSONArray locations, JSONArray range);
   FeatureCollection getGeoCodes(String geocode);
+  FeatureCollection getDirections(Point origin, Point destination, String profile);
 }
